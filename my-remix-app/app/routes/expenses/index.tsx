9422278084ce89593f207002debda3e026e0a246
Sirojjuddin Apendi <60147575+siroj05@users.noreply.github.com>
2023-02-27 02:32:28 +0000

    init component

---
+++ my-remix-app/app/routes/expenses/index.tsx
@@ -0,0 +1,5 @@
+export default function ExpensesPages(){
+    return (
+        <h1>Expenses Pages</h1>
+    )
+}
---
+++ my-remix-app/app/routes/expenses/raw.tsx
@@ -0,0 +1,5 @@
+export default function ExpensesRawData(){
+    return(
+        <h1>Expenses Raw Data</h1>
+    )
+}
